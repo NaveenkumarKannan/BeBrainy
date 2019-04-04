1994d7fa422f5bc3e0a33ae6cf2723ed2fa7cd03
NaveenkumarKannan <naveenkumar70271@gmail.com>
2019-04-04 23:59:57 +0530

    add all files

---
+++ app/src/main/java/in/bebrainy/bebrainy/MainActivity.java
@@ -0,0 +1,114 @@
+package in.bebrainy.bebrainy;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog loading;
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loading = ProgressDialog.show(this, "Loading Page...","Please Wait...",true,true);
+
+        Bundle bundle = getIntent().getExtras();
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new myWebClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            /*
+            String url = "http://www.google.com";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+            */
+            webView.getSettings().setSafeBrowsingEnabled(true);
+        }
+        webView.loadUrl("https://www.bebrainy.in");
+        //webView.loadUrl(bundle.getString("Link"));
+
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+//        Log.e("NewsFeed", NewsFeed);
+        /**
+         * Enabling zoom-in controls
+         * */
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(true);
+    }
+
+    /*
+    @Override
+    public void onBackPressed() {
+        if (NewsFeed.equals("NewsFeed")) {
+            Intent intent = new Intent(WebViewActivity.this,NewsFeedActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }else {
+            Intent intent = new Intent(WebViewActivity.this,MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+    */
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+
+            view.loadUrl(url);
+            return true;
+
+        }
+        @Override
+        public void onReceivedError(WebView view, int errorCode,
+                                    String description, String failingUrl) {
+            // TODO Auto-generated method stub
+            Toast.makeText(MainActivity.this, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            //super.onReceivedError(view, errorCode, description, failingUrl);
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+            loading.dismiss();
+        }
+    }
+
+    // To handle "Back" key press event for WebView to go back to previous screen.
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        else
+        {
+            finish();
+            return true;
+        }
+    }
+}
